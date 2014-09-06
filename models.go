f502cbff492bfc8c5933bfe283516bc85f70dcd6
Nat Welch <nat@natwelch.com>
2014-09-06 17:12:19 +0000

    base model def

---
+++ models.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"time"
+)
+
+type Entry struct {
+	id       uint64
+	title    string // optional
+	content  string // Markdown
+	datetime time.Time
+	created  time.Time
+	modified time.Time
+	tags     []string
+	// TODO(icco): Define a meta field that is a json hash of extra data
+}
