5f7e193f11463dcd8c5263d043e21df926846fee
Nat Welch <nat@natwelch.com>
2014-10-21 23:29:14 +0000

    messing around with embedly

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,9 @@
 $('.markdown a').embedly({
   key: 'be853af8968a408eb7ec368d2019614a',
-  query: {maxwidth: 600},
-  'method':'after'
+  query: {
+    maxwidth: 400,
+    words: 20,
+  },
+  secure: true,
+  method: 'afterParent'
 });
