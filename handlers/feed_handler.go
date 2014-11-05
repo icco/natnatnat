8b80eefcdb5dfdaf5cf0ddb91c840f7ae089263d
Nat Welch <nat@natwelch.com>
2014-11-05 10:19:38 +0000

    Add rss and atom summary

--- handlers/feed_handler.go
+++ handlers/feed_handler.go
@@ -37,6 +37,34 @@ func buildFeed(c appengine.Context, entries *[]models.Entry) *feeds.Feed {
 	return feed
 }
 
+func buildSummary(c appengine.Context, entries *[]models.Entry) *feeds.Feed {
+	now := time.Now()
+	me := &feeds.Author{"Nat Welch", "nat@natwelch.com"}
+	feed := &feeds.Feed{
+		Title:       "Nat? Nat. Nat! - Summaries",
+		Link:        &feeds.Link{Href: "https://writing.natwelch.com"},
+		Description: "Thoughts from Nat about stuff",
+		Author:      me,
+		Created:     now,
+	}
+
+	feed.Items = []*feeds.Item{}
+	for _, v := range *entries {
+		if v.Public {
+			feed.Items = append(feed.Items, &feeds.Item{
+				Title:       v.Title,
+				Link:        &feeds.Link{Href: v.Url()},
+				Description: v.Summary(),
+				Author:      me,
+				Created:     v.Datetime,
+				Updated:     v.Modified,
+			})
+		}
+	}
+
+	return feed
+}
+
 func FeedAtomHandler(w traffic.ResponseWriter, r *traffic.Request) {
 	c := appengine.NewContext(r.Request)
 	entries, err := models.AllPosts(c)
@@ -63,6 +91,42 @@ func FeedRssHandler(w traffic.ResponseWriter, r *traffic.Request) {
 		return
 	}
 	feed := buildFeed(c, entries)
+	rss, err := feed.ToRss()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/rss+xml; charset=utf-8")
+	w.WriteText("%s", rss)
+	return
+}
+
+func SummaryAtomHandler(w traffic.ResponseWriter, r *traffic.Request) {
+	c := appengine.NewContext(r.Request)
+	entries, err := models.AllPosts(c)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	feed := buildSummary(c, entries)
+	atom, err := feed.ToAtom()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/atom+xml; charset=utf-8")
+	w.WriteText("%s", atom)
+	return
+}
+
+func SummaryRssHandler(w traffic.ResponseWriter, r *traffic.Request) {
+	c := appengine.NewContext(r.Request)
+	entries, err := models.AllPosts(c)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	feed := buildSummary(c, entries)
 	rss, err := feed.ToRss()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
