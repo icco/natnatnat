27e96792054390c92b8c1e1f27080b8067eed831
Nat Welch <nat@natwelch.com>
2015-04-09 18:06:18 +0000

    move stats handler to seperate file

--- handlers/root_handler.go
+++ handlers/root_handler.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"net/http"
-	"strings"
 	"time"
 
 	"appengine"
@@ -34,39 +33,6 @@ func AboutHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
 func UnimplementedHandler(w traffic.ResponseWriter, r *traffic.Request) {
 	http.Error(w, "Sorry, I haven't implemented this yet", 500)
-}
-
-type StatsData struct {
-	Posts        int
-	WordsPerPost float64
-	PostsPerDay  float64
-	IsAdmin      bool
-}
-
-func StatsHandler(w traffic.ResponseWriter, r *traffic.Request) {
-	c := appengine.NewContext(r.Request)
-	entries, err := models.AllPosts(c)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	postCount := len(*entries)
-	oldestPost := (*entries)[postCount-1]
-	dayCount := time.Since(oldestPost.Datetime).Hours() / 24
-
-	words := 0
-	for _, p := range *entries {
-		words += len(strings.Fields(p.Content))
-		words += len(strings.Fields(p.Title))
-	}
-
-	data := &StatsData{
-		Posts:        postCount,
-		PostsPerDay:  float64(postCount) / dayCount,
-		WordsPerPost: float64(words) / float64(postCount),
-		IsAdmin:      user.IsAdmin(c),
-	}
-	w.Render("stats", data)
 }
 
 type ArchiveData struct {
---
+++ handlers/stats_handler.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"appengine"
+	"appengine/user"
+
+	"github.com/icco/natnatnat/models"
+	"github.com/pilu/traffic"
+)
+
+type StatsData struct {
+	Posts        int
+	WordsPerPost float64
+	PostsPerDay  float64
+	IsAdmin      bool
+}
+
+func StatsHandler(w traffic.ResponseWriter, r *traffic.Request) {
+	c := appengine.NewContext(r.Request)
+	entries, err := models.AllPosts(c)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	postCount := len(*entries)
+	oldestPost := (*entries)[postCount-1]
+	dayCount := time.Since(oldestPost.Datetime).Hours() / 24
+
+	words := 0
+	for _, p := range *entries {
+		words += len(strings.Fields(p.Content))
+		words += len(strings.Fields(p.Title))
+	}
+
+	data := &StatsData{
+		Posts:        postCount,
+		PostsPerDay:  float64(postCount) / dayCount,
+		WordsPerPost: float64(words) / float64(postCount),
+		IsAdmin:      user.IsAdmin(c),
+	}
+	w.Render("stats", data)
+}
