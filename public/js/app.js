966cd092c44b86f95dddb93c694f4a8721dee144
Nat Welch <nat@natwelch.com>
2015-04-10 00:34:01 +0000

    cut and paste.

--- public/js/app.js
+++ public/js/app.js
@@ -55,33 +55,86 @@ $(document).ready(function() {
 
   // Stats Graph
   if ($("#statsgraph").length) {
+    // Chart dimensions
+    var m = [20, 80, 20, 80]; // Margins
+    var w = 960 - m[1] - m[3];
+    var h = 150 - m[0] - m[2];
+
+    // Scales. Nice functions which auto resize things.
+    // Also defines the ranges for the graph (top and bottom numbers)
+    var x = d3.time.scale().range([0, w]);
+    var y = d3.scale.linear().range([h, 0]);
+
+    var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+    var yAxis = d3.svg.axis().scale(y).ticks(4).orient("right");
+
+    // An area generator, for the light fill.
+    var area = d3.svg.area()
+      .interpolate("basis")
+      .x(function(d) { return x(d.x); })
+      .y0(h)
+      .y1(function(d) { return y(d.y); });
+
+    var line = d3.svg.line()
+      .interpolate("basis")
+      .x(function(d) { return x(d.x); })
+      .y(function(d) { return y(d.y); });
+
+    var color = "black";
+
     d3.json("/posts.json", function(error, data) {
-      var w = 500;
-      var h = 500;
-      var canvas = d3.select("#statsgraph")
-        .append("svg")
+      var values = data.map(function(d) {
+        return { x: new Date(d.date), y: +d.id };
+      });
+
+      // Compute the minimum and maximum date, and the maximum y value.
+      x.domain([new Date(data[0].date), new Date(data[data.length - 1].date)]);
+      y.domain([0, d3.max(values, function(d) { return d.y; })]).nice();
+
+      // Add an SVG element with the desired dimensions and margin.
+      var svg = d3.select("#statsgraph").append("svg:svg")
+        .attr("width", w + m[1] + m[3])
+        .attr("height", h + m[0] + m[2])
+        .append("svg:g")
+        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+      // TODO(icco): get this to work.
+      var barPadding = 1;
+      svg.selectAll("rect")
+        .data(values)
+        .enter()
+        .append("rect")
+        .attr("fill", color)
+        .attr("x", function(d) { return x(d.x); })
+        .attr("y", function(d) { return y(d.y); })
+        .attr("width", 1)
+        .attr("height", function(d) { return h - y(d.y); });
+
+      // Add the clip path.
+      svg.append("svg:clipPath")
+        .attr("id", "clip")
+        .append("svg:rect")
         .attr("width", w)
-        .attr("height", h)
-        .attr("border", "black");
+        .attr("height", h);
 
-      var group = canvas.append("g")
-        .attr("transform", "translate(100,10)");
+      // Add the x-axis.
+      svg.append("svg:g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + h + ")")
+        .call(xAxis);
 
-      var x = d3.time.scale().range([0, w]);
-      var y = d3.scale.linear().range([0, h]);
+      // Add the y-axis.
+      svg.append("svg:g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + w + ",0)")
+        .call(yAxis);
 
-      var line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(d) { return console.log(d); x(new Date(d.date)); })
-        .y(function(d) { return y(d.id); }); 
-
-      group.selectAll("path")
-        .data(data).enter()
-        .append("path")
-        .attr("d", function(d) { console.log(y(d.id), y(new Date(d.date)), line(d)); return line(d); })
-        .attr("fill", "none")
-        .attr("stroke", "green")
-        .attr("stroke-width", 3);
+      // Add a small label for the name.
+      svg.append("svg:text")
+        .attr("x", w - 6)
+        .attr("y", m[0] - 12)
+        .attr("text-anchor", "end")
+        .text("commits/day");
     });
   }
 });
