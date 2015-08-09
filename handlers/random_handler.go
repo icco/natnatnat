0833df473af13cc51f9420860795f846f301f201
Nat Welch <nat@natwelch.com>
2015-08-09 22:40:56 +0000

    bringing back the handler

---
+++ handlers/random_handler.go
@@ -0,0 +1,61 @@
+package handlers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/user"
+
+	"github.com/icco/natnatnat/models"
+	"github.com/pilu/traffic"
+)
+
+type ImportStruct struct {
+	Datetime time.Time `json:"date"`
+	Text     string    `json:"text"`
+	Title    string    `json:"title"`
+	Link     string    `josn:"link"`
+}
+
+func ImportPseudowebHandler(w traffic.ResponseWriter, r *traffic.Request) {
+	c := appengine.NewContext(r.Request)
+	u := user.Current(c)
+	if u == nil {
+		url, _ := user.LoginURL(c, "/")
+		http.Redirect(w, r.Request, url, 302)
+		return
+	} else {
+		log.Infof(c, "Logged in as: %s", u.String())
+	}
+
+	if u != nil && !user.IsAdmin(c) {
+		http.Error(w, errors.New("Not a valid user.").Error(), 403)
+		return
+	} else {
+		file, err := ioutil.ReadFile("tmp/all.json")
+		if err != nil {
+			e := fmt.Sprintf("File error: %v", err)
+			log.Errorf(c, e)
+			http.Error(w, e, 500)
+			return
+		}
+
+		var data []ImportStruct
+		json.Unmarshal(file, &data)
+		log.Debugf(c, "Loaded: %v", data)
+
+		for p := range data {
+			p.Text = fmt.Sprintf("Posted originally at %s.\n\n%s", p.Link, p.Text)
+			e := models.NewEntry(p.Title, p.Text, p.Datetime, true, []string{})
+			e.Save()
+		}
+
+		w.WriteText("Finished.")
+	}
+}
