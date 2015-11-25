b8b680baa7cb50fe399641553cab10f1c6862724
Nat Welch <nat@natwelch.com>
2015-11-25 05:16:27 +0000

    progress

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,13 @@ module.exports = {
     loaders: [
       { test: /\.scss$/, loaders: ["style", "css?sourceMap", "sass?sourceMap"] },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
     ]
   }
 };
