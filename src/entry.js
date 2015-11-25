ae92aaa56674bcb5e250f8ab573ca0f2c39aaf8c
Nat Welch <nat@natwelch.com>
2015-11-25 13:14:51 +0000

    progress

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,21 @@
-var app = require('./js/app.js');
-require('./scss/tachyons.css');
-require('./scss/style.scss');
-require('./img/natwelchlogo.png');
+window.onload = function() {
+  var time = require('./js/time.js');
+  time();
 
-app();
+  var embed = require('./js/embedly.js');
+  embed();
+
+  var links = require('./js/link_adder.js');
+  links();
+
+  var md = require('./js/markdown.js');
+  md();
+};
 
 // https://highlightjs.org/usage/
 var hljs = require('highlight.js');
 hljs.initHighlightingOnLoad();
+
+require('./scss/tachyons.css');
+require('./scss/style.scss');
+require('./img/natwelchlogo.png');
---
+++ src/js/link_adder.js
@@ -0,0 +1,17 @@
+// Takes a list of links and makes it so when you click them the appear as
+// markdown in a textbox.
+var LinkAdder = function() {
+  var $ = require('jquery');
+  $('li.link').each(function(i, el) {
+    $(el).children('a.adder').click(function() {
+      link = $(el).children('a.actual')[0];
+      title = $(link).text();
+      tags = $(this).data('tags');
+      url =  $(link).attr('href');
+      mkd = "\n[" + title + "](" + url + ")\n\n" + tags;
+      ta = $('textarea[name="text"]');
+      ta.val(ta.val() + mkd);
+    });
+  });
+};
+module.exports = LinkAdder;
