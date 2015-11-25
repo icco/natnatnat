dd508c6314556d733b7b2a4fe076dac5486e548b
Nat Welch <nat@natwelch.com>
2015-11-25 05:17:35 +0000

    optimized images

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'file?hash=sha512&digest=hex&name=public/img/[hash].[ext]',
           'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
         ]
       }
