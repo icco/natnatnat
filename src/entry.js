3c3e930217335a94fa456b59b574906165baa827
Nat Welch <nat@natwelch.com>
2015-11-25 13:06:23 +0000

    playing around with webpack

--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,10 @@
-var $ = require('jquery');
-var moment = require('moment');
-var hl = require('highlight.js');
-var d3 = require('d3');
-
-require('./js/app.js');
+var app = require('./js/app.js');
 require('./scss/tachyons.css');
 require('./scss/style.scss');
 require('./img/natwelchlogo.png');
+
+app();
+
+// https://highlightjs.org/usage/
+var hljs = require('highlight.js');
+hljs.initHighlightingOnLoad();
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
+// Parses links and turns them into embeds.
+//
 // https://github.com/embedly/embedly-jquery
 // https://wordpress.stackexchange.com/questions/15445/is-there-a-built-in-function-to-see-if-a-urlis-oembed-compatible
-$(document).ready(function() {
-  // Default
-  var defaultUrlRe = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+var Embedly = function() {
+  var $ = require('jquery');
 
   // Youtube and Vimeo only
   var videoUrlRe = /https?:\/\/(www\.)?(youtu|vimeo|soundcloud).+/i;
@@ -39,7 +40,13 @@ $(document).ready(function() {
       prent.append(addon);
     });
   });
+};
+module.exports = Embedly;
 
+// Takes a list of links and makes it so when you click them the appear as
+// markdown in a textbox.
+var LinkAdder = function() {
+  var $ = require('jquery');
   $('li.link').each(function(i, el) {
     $(el).children('a.adder').click(function() {
       link = $(el).children('a.actual')[0];
@@ -51,12 +58,16 @@ $(document).ready(function() {
       ta.val(ta.val() + mkd);
     });
   });
+};
+module.exports = LinkAdder;
 
-  // Markdown Preview
+// Markdown Preview generator.
+var MarkdownPreview = function() {
+  var $ = require('jquery');
   var md_text_name = "textarea[name=text]";
   if ($(md_text_name).length) {
     $(md_text_name).bind('input propertychange', function() {
-      jQuery.post('/md', {'text': $(this).val()}, function (data) {
+      $.post('/md', {'text': $(this).val()}, function (data) {
         $('#rendered').html(data);
       });
     });
@@ -72,94 +83,14 @@ $(document).ready(function() {
   $('.show_hide').click(function() {
     $("#rendered").slideToggle();
   });
+};
+module.exports = MarkdownPreview;
 
-  // Stats Graph
-  if ($("#statsgraph").length && false) {
-    // Chart dimensions
-    var m = [20, 80, 20, 80]; // Margins
-    var w = 960 - m[1] - m[3];
-    var h = 150 - m[0] - m[2];
-
-    // Scales. Nice functions which auto resize things.
-    // Also defines the ranges for the graph (top and bottom numbers)
-    var x = d3.time.scale().range([0, w]);
-    var y = d3.scale.linear().range([h, 0]);
-
-    var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-    var yAxis = d3.svg.axis().scale(y).ticks(4).orient("right");
-
-    // An area generator, for the light fill.
-    var area = d3.svg.area()
-      .interpolate("basis")
-      .x(function(d) { return x(d.x); })
-      .y0(h)
-      .y1(function(d) { return y(d.y); });
-
-    var line = d3.svg.line()
-      .interpolate("basis")
-      .x(function(d) { return x(d.x); })
-      .y(function(d) { return y(d.y); });
-
-    var color = "black";
-
-    d3.json("/posts.json", function(error, data) {
-      var values = data.map(function(d) {
-        return { x: new Date(d.date), y: +d.id };
-      });
-
-      // Compute the minimum and maximum date, and the maximum y value.
-      x.domain([new Date(data[0].date), new Date(data[data.length - 1].date)]);
-      y.domain([0, d3.max(values, function(d) { return d.y; })]).nice();
-
-      // Add an SVG element with the desired dimensions and margin.
-      var svg = d3.select("#statsgraph").append("svg:svg")
-        .attr("width", w + m[1] + m[3])
-        .attr("height", h + m[0] + m[2])
-        .append("svg:g")
-        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-      // TODO(icco): get this to work.
-      var barPadding = 1;
-      svg.selectAll("rect")
-        .data(values)
-        .enter()
-        .append("rect")
-        .attr("fill", color)
-        .attr("x", function(d) { return x(d.x); })
-        .attr("y", function(d) { return y(d.y); })
-        .attr("width", 1)
-        .attr("height", function(d) { return h - y(d.y); });
-
-      // Add the clip path.
-      svg.append("svg:clipPath")
-        .attr("id", "clip")
-        .append("svg:rect")
-        .attr("width", w)
-        .attr("height", h);
-
-      // Add the x-axis.
-      svg.append("svg:g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + h + ")")
-        .call(xAxis);
-
-      // Add the y-axis.
-      svg.append("svg:g")
-        .attr("class", "y axis")
-        .attr("transform", "translate(" + w + ",0)")
-        .call(yAxis);
-
-      // Add a small label for the name.
-      svg.append("svg:text")
-        .attr("x", w - 6)
-        .attr("y", m[0] - 12)
-        .attr("text-anchor", "end")
-        .text("commits/day");
-    });
-  }
-
-  // Change the time elements to be relative times.
-  // Requires moment.js
+// Change the time elements to be relative times.
+// Requires moment.js
+var RelativeTimes = function() {
+  var $ = require('jquery');
+  var moment = require('moment');
   $('time').each(function(i, el) {
     var t = moment($(el).attr('datetime'), "YYYY-MM-DDTHH:mm:ss.SSSSSSZ");
     $(el).text(t.fromNow());
@@ -170,7 +101,5 @@ $(document).ready(function() {
     var t = moment($(el).text(), "YYYY-MM-DD HH:mm:ssZ");
     $(el).text(t.utc().format("MMMM Do YYYY"));
   });
-
-  // https://highlightjs.org/usage/
-  hljs.initHighlightingOnLoad();
-});
+};
+module.exports = RelativeTimes;
