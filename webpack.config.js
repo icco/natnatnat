5a059ef6bdc17136d6bfd16e0e9a172bdde0fdae
Nat Welch <nat@natwelch.com>
2015-11-25 14:28:42 +0000

    moving it around

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   entry: "./src/entry.js",
   output: {
-    path: __dirname,
-    filename: "public/js/bundle.js"
+    path: "./public/",
+    filename: "js/bundle.js"
   },
   devtool: "source-map",
   module: {
     loaders: [
       { test: /\.scss$/, loaders: ["style", "css?sourceMap", "sass?sourceMap"] },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-          'file?hash=sha512&digest=hex&name=public/img/[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          "file?hash=sha512&digest=hex&name=img/[hash].[ext]",
+          "image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false"
         ]
       }
     ]
