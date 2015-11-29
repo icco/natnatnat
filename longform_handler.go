edb3afe4fbc92eeab8da857f76f78f2648850d8b
Nat Welch <nat@natwelch.com>
2015-11-29 00:59:40 +0000

    iterate over posts

--- longform_handler.go
+++ longform_handler.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/pilu/traffic"
-
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/taskqueue"
+	//"gopkg.in/yaml.v2"
 )
 
 func LongformQueueHandler(w traffic.ResponseWriter, r *traffic.Request) {
@@ -24,9 +27,34 @@ func LongformQueueHandler(w traffic.ResponseWriter, r *traffic.Request) {
 }
 
 func LongformWorkHandler(w traffic.ResponseWriter, r *traffic.Request) {
-	// c := appengine.NewContext(r.Request)
-	// Stub:
+	c := appengine.NewContext(r.Request)
+
 	// Read drafts from disk
+	drafts, err := ioutil.ReadDir("./longform/drafts")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	log.Warningf(c, "Drafts: %+v", drafts)
+
 	// Iterate through, create entries for those that don't exist, update those that do
-	// Read posts from disk, create entries for those that don't exist, update those that do
+	for _, file := range drafts {
+		if strings.HasPrefix(file.Name(), "20") {
+			log.Infof(c, "Draft: %+v", file.Name())
+		}
+	}
+
+	// Read posts from disk,
+	posts, err := ioutil.ReadDir("./longform/posts")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// create entries for those that don't exist, update those that do
+	for _, file := range posts {
+		if strings.HasPrefix(file.Name(), "20") {
+			log.Infof(c, "Post: %+v", file.Name())
+		}
+	}
 }
