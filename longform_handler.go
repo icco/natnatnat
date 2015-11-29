ad7f721272a2a50af7a229f32e77b3128a766265
Nat Welch <nat@natwelch.com>
2015-11-29 15:07:08 +0000

    progress

--- longform_handler.go
+++ longform_handler.go
@@ -7,8 +7,10 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/pilu/traffic"
+	"github.com/spf13/cast"
 	"github.com/spf13/hugo/parser"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
@@ -55,7 +57,14 @@ func LongformWorkHandler(w traffic.ResponseWriter, r *traffic.Request) {
 				http.Error(w, err.Error(), 500)
 				return
 			}
-			meta, err := p.Metadata()
+			meta_uncast, err := p.Metadata()
+			if err != nil {
+				log.Errorf(c, "Error reading file: %v", err.Error())
+				http.Error(w, err.Error(), 500)
+				return
+			}
+
+			meta, err := cast.ToStringMapStringE(meta_uncast)
 			if err != nil {
 				log.Errorf(c, "Error reading file: %v", err.Error())
 				http.Error(w, err.Error(), 500)
@@ -66,9 +75,10 @@ func LongformWorkHandler(w traffic.ResponseWriter, r *traffic.Request) {
 			entry, err := GetLongform(c, file.Name())
 			if entry == nil {
 				// TODO: datetime
-				datetime := time.Date(year, month, day, hour, min, 0, 0, time.UTC)
-
-				entry = NewEntry(meta["title"], p.Content(), datetime, true, []string{})
+				log.Debugf(c, "Meta: %+v", meta)
+				posted_at := time.Now()
+				datetime := time.Date(posted_at.Year(), posted_at.Month(), posted_at.Day(), posted_at.Hour(), posted_at.Minute(), 0, 0, time.UTC)
+				entry = NewEntry(meta["title"], string(p.Content()), datetime, true, []string{})
 			}
 			entry.Draft = true
 			entry.Save(c)
