8968c6add9b0eed9b1ad04023c2dc9fa9f864513
Nat Welch <nat@natwelch.com>
2016-02-03 01:54:01 +0000

    memcache

--- stats_handler.go
+++ stats_handler.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
 	"time"
 
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/user"
-
 	"github.com/pilu/traffic"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/memcache"
+	"google.golang.org/appengine/user"
 )
 
 type StatsData struct {
@@ -24,37 +26,67 @@ type StatsData struct {
 
 func StatsHandler(w traffic.ResponseWriter, r *traffic.Request) {
 	c := appengine.NewContext(r.Request)
-	entries, err := AllPosts(c)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	postCount := len(*entries)
-	oldestPost := (*entries)[postCount-1]
-	dayCount := time.Since(oldestPost.Datetime).Hours() / 24.0
 
-	words := 0
-	for _, p := range *entries {
-		words += len(strings.Fields(p.Content))
-		words += len(strings.Fields(p.Title))
+	var data *StatsData
+	if json_data, err := memcache.Get(c, "stats_data"); err == memcache.ErrCacheMiss {
+		entries, err := AllPosts(c)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		postCount := len(*entries)
+		oldestPost := (*entries)[postCount-1]
+		dayCount := time.Since(oldestPost.Datetime).Hours() / 24.0
+
+		words := 0
+		for _, p := range *entries {
+			words += len(strings.Fields(p.Content))
+			words += len(strings.Fields(p.Title))
+		}
+
+		links, err := AllLinks(c)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		readLinks := len(*links)
+
+		data = &StatsData{
+			Posts:        postCount,
+			PostsPerDay:  float64(postCount) / dayCount,
+			WordsPerPost: float64(words) / float64(postCount),
+			WordsPerDay:  float64(words) / dayCount,
+			DaysSince:    dayCount,
+			LinksPerDay:  float64(readLinks) / dayCount,
+		}
+
+		b, err := json.Marshal(data)
+		if err != nil {
+			log.Errorf(c, err.Error())
+			http.Error(w, err.Error(), 500)
+		}
+		item := &memcache.Item{
+			Key:   "stats_data",
+			Value: b,
+		}
+
+		// Set the item, unconditionally
+		if err := memcache.Set(c, item); err != nil {
+			log.Errorf(c, "error setting item: %v", err)
+		}
+	} else if err != nil {
+		log.Errorf(c, "error getting item: %v", err)
+	} else {
+		err := json.Unmarshal(json_data.Value, &data)
+		if err != nil {
+			log.Errorf(c, err.Error())
+			http.Error(w, err.Error(), 500)
+			return
+		}
 	}
 
-	links, err := AllLinks(c)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	readLinks := len(*links)
-
-	data := &StatsData{
-		Posts:        postCount,
-		PostsPerDay:  float64(postCount) / dayCount,
-		WordsPerPost: float64(words) / float64(postCount),
-		WordsPerDay:  float64(words) / dayCount,
-		DaysSince:    dayCount,
-		IsAdmin:      user.IsAdmin(c),
-		LinksPerDay:  float64(readLinks) / dayCount,
-	}
+	data.IsAdmin = user.IsAdmin(c)
 	w.Render("stats", data)
 }
 
